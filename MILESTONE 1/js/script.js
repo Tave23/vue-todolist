baf6bda6b0972cafa2c976b52ab0204d77a2b40c
Davide Tavella <tavella01@gmail.com>
2021-11-12 15:20:10 +0100

    milestone 1

---
+++ MILESTONE 1/js/script.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+
+   el: '#app',
+
+   data:{
+   // lista delle cose da fare già impostate
+       todos:[
+            {
+               item:'Lavare i piatti',      
+            },
+            {
+               item:'Gonfiare la bici',
+            },
+            {
+               item:'Stendere i panni',
+            },
+            {
+               item:'Comprare regalo',
+            },
+   ],
+   
+   
+     
+   },
+
+   mounted(){
+
+      
+
+   },
+
+   methods:{
+
+      
+
+      
+       
+   }
+
+})
